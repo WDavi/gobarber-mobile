8a4eccbc15af35d192f8d25041b90e2bab97d109
Davi Wendt <daviwendt@gmail.com>
2020-05-05 19:36:56 -0300

    Add SignIn, SignUp and navigation

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,13 +1,54 @@
 import React from 'react';
-import { Text } from 'react-native';
+import {
+  Image,
+  View,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
+} from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/Feather';
 
-import { Container } from './styles';
+import logoImg from '../../assets/logo.png';
+
+import Button from '../../components/Button';
+import Input from '../../components/Input';
+
+import { Container, Title, GoToLogon, GoToLogonText } from './styles';
 
 const SignUp: React.FC = () => {
+  const navigation = useNavigation();
   return (
-    <Container>
-      <Text>SignUp</Text>
-    </Container>
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <ScrollView
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{ flex: 1 }}
+        >
+          <Container>
+            <Image source={logoImg} />
+
+            <View>
+              <Title>Crie sua conta</Title>
+            </View>
+
+            <Input name="name" icon="user" placeholder="Nome" />
+            <Input name="email" icon="mail" placeholder="E-mail" />
+            <Input name="password" icon="lock" placeholder="Password" />
+
+            <Button onPress={() => console.log('clico')}>Entrar</Button>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+      <GoToLogon onPress={() => navigation.goBack()}>
+        <Icon name="arrow-left" size={20} color="#fff" />
+        <GoToLogonText>Voltar para logon</GoToLogonText>
+      </GoToLogon>
+    </>
   );
 };
 
--- src/pages/SignUp/styles.ts
+++ src/pages/SignUp/styles.ts
@@ -1,3 +1,42 @@
 import styled from 'styled-components/native';
+import { Platform } from 'react-native';
+import { getBottomSpace } from 'react-native-iphone-x-helper';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  padding: 0 30px ${Platform.OS === 'android' ? 175 : 40}px;
+`;
+
+export const Title = styled.Text`
+  margin: 64px 0 24px;
+  font-family: 'RobotoSlab-Medium';
+  color: #f4ede8;
+  font-size: 20px;
+`;
+
+export const GoToLogon = styled.TouchableOpacity`
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+
+  background: #312e38;
+
+  border-top-width: 1px;
+  border-color: #232129;
+
+  padding: 16px 0 ${16 + getBottomSpace()}px;
+`;
+
+export const GoToLogonText = styled.Text`
+  margin-left: 8px;
+  font-family: 'RobotoSlab-Regular';
+  color: #fff;
+  font-size: 20px;
+`;
